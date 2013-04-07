a6587cbe099ee57424cdf0216fd0142e3a1b72da
Stefan Gehrig <stefan.gehrig.hn@googlemail.com>
2013-04-07 14:58:02 +0200

    switched to ant-based build system

--- build/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-PATH_SCRIPT=$(cd "$(dirname "$0")"; pwd)
-
-$PATH_SCRIPT/generate-docs.sh
-$PATH_SCRIPT/compile.sh
--- build/generate-docs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-PATH_SCRIPT=$(cd "$(dirname "$0")"; pwd)
-
-jsduck --config $PATH_SCRIPT/docs.json
